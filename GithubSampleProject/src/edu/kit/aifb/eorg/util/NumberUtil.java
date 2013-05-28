dc280a07ddd1a236db14139ec3fcbba0f1b2beab
Bugra <bugra.derre@kit.edu>
2013-05-29 01:08:57 +0200

    Insert isValid operation

--- GithubSampleProject/src/edu/kit/aifb/eorg/util/NumberUtil.java
+++ GithubSampleProject/src/edu/kit/aifb/eorg/util/NumberUtil.java
@@ -5,5 +5,13 @@ public class NumberUtil {
 	public boolean isPositive(int number) {
 		return number > 0;
 	}
+	
+	public boolean isNegative(int number) {
+		return number < 0;
+	}
+	
+	public boolean isValidMessage(String message) {
+		return message != null && message.length() > 0;
+	}
 
 }
