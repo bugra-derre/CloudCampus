dada9842997bdd982b88c0ad9211b1aaac80d9ac
Bugra <bugra.derre@kit.edu>
2013-05-29 01:08:57 +0200

    Fixed the broken email address validation.

--- GithubSampleProject/src/edu/kit/aifb/eorg/util/NumberUtil.java
+++ GithubSampleProject/src/edu/kit/aifb/eorg/util/NumberUtil.java
@@ -3,7 +3,15 @@ package edu.kit.aifb.eorg.util;
 public class NumberUtil {
 	
 	public boolean isPositive(int number) {
-		return number > 0;
+		return number >= 0;
+	}
+	
+	public boolean isNegative(int number) {
+		return number < 0;
+	}
+	
+	public boolean isValidMessage(String message) {
+		return message != null && message.length() > 0;
 	}
 
 }
